c7dde0613b4392d13b9118be8176a9d53d0dd73f
Matrix <166641279+matrixxdev@users.noreply.github.com>
2025-01-29 15:47:34 +0100

    Uploaded fabric mod template files

---
+++ src/main/java/net/matrix/bbdamage/BBDamageDataGenerator.java
@@ -0,0 +1,11 @@
+package net.matrix.bbdamage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BBDamageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
